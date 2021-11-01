2b0abe0953b6b3be749ea24073de685ed2f382e9
Daachury <15achury02@gmail.com>
2021-10-31 20:02:17 -0500

    Add service find by id vehiculo

--- src/main/java/com/motors/sport/app/controller/VehiculosController.java
+++ src/main/java/com/motors/sport/app/controller/VehiculosController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,5 +37,9 @@ public class VehiculosController {
 		return new ResponseEntity<List<Vehiculo>>(repository.findAll(),HttpStatus.OK);
 	}
 
+	@GetMapping("/allVehiculo/{id}")
+	public ResponseEntity<Vehiculo> readvehiculo(@PathVariable Long id){
+		return new ResponseEntity<Vehiculo>(repository.findById(id).get(),HttpStatus.OK);
+	}
 	
 }
